cb6dfd6a94c32b759fb01dae7c9609c5b8a0e377
saladinjake <juwavictor@gmail.com>
2022-06-30 16:39:34 +0100

    preetier fixes

--- frontend/src/enterprise-version/core/routes/router.js
+++ frontend/src/enterprise-version/core/routes/router.js
@@ -1,20 +1,16 @@
 /* eslint-disable import/no-named-as-default */
 
-
-
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
+import DriverRoutes from './Driver/DriverRoutes';
 
-import DriverRoutes from "./Driver/DriverRoutes"
+const AppRoutes = () => {
+  return (
+    <>
+      <DriverRoutes />
+    </>
+  );
+};
 
-const AppRoutes = () =>{
-    return (
-      <>
-         <DriverRoutes/>
-      </>
-    )
-}
-
-export default AppRoutes
-
+export default AppRoutes;
--- frontend/src/enterprise-version/views/Drivers/Driver/Login.js
+++ frontend/src/enterprise-version/views/Drivers/Driver/Login.js
@@ -1,44 +1,46 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000'
+const BASE_URL = 'http://localhost:8000';
 
 const Login = props => {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSubmit = ev => {
-    ev.preventDefault()
+    ev.preventDefault();
 
-    axios.post(`${BASE_URL}/login`, {email, password})
-    .then( res => {
-      if(res.data.success){
-        sessionStorage.setItem('token', JSON.stringify(res.data.token))
-        sessionStorage.setItem('user', JSON.stringify(res.data.user))
-        axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.token}`
-        props.history.push('/maps')
-      }
-    })
-    .catch(console.error)
-  }
+    axios
+      .post(`${BASE_URL}/login`, { email, password })
+      .then(res => {
+        if (res.data.success) {
+          sessionStorage.setItem('token', JSON.stringify(res.data.token));
+          sessionStorage.setItem('user', JSON.stringify(res.data.user));
+          axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.token}`;
+          props.history.push('/maps');
+        }
+      })
+      .catch(console.error);
+  };
 
-  return(
-    <div  className="login-wrapper">
+  return (
+    <div className="login-wrapper">
       <h2>GOOM Logistics</h2>
       <div>
         <h3>Log into your account</h3>
         <form onSubmit={handleSubmit}>
-          <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} required />
-          <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} required />
+          <input type="email" name="email" onChange={e => setEmail(e.target.value)} required />
+          <input
+            type="password"
+            name="password"
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
           <button>Login</button>
         </form>
       </div>
-      
-     
-  
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
